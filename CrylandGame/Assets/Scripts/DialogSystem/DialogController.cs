6cd1a52ffa8a7119245cf84977db203c70ce32b4
impoopydoopy <113069987+impoopydoopy@users.noreply.github.com>
2023-08-26 15:28:20 +0300

    Implemented debug StartDialog method

--- CrylandGame/Assets/Scripts/DialogSystem/DialogController.cs
+++ CrylandGame/Assets/Scripts/DialogSystem/DialogController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
 using NPC;
 using UnityEngine;
 
@@ -7,15 +10,37 @@ namespace DialogSystem
 {
     public class DialogController : MonoBehaviour
     {
+        
         public static DialogController instance;
+        public Character mockNPC;
 
         private void Awake() {
             instance = this;
         }
 
+        private void Start()
+        {
+            StartDialog(mockNPC, 0);
+            StartDialog(mockNPC, 1);
+            StartDialog(mockNPC, 2);
+        }
+
         public void StartDialog(Character character, int currentDay)
         {
-            // TODO: implement
+            Conversations conversations = character.conversations;
+            if (currentDay >= conversations.ConversationList.Count)
+            {
+                Debug.LogError("No such day!");
+                return;
+            }
+                
+            List<Dialog> dialogs = conversations.ConversationList[1].Dialogs;
+            Debug.Log("Day: " + currentDay);
+            foreach (var dialog in dialogs)
+            {
+                Debug.Log("Phrase: " + dialog.Phrase);
+                Debug.Log("Answer: " + dialog.Answer);
+            }
         }
     }
 }
--- CrylandGame/Assets/Scripts/NPC/Character.cs
+++ CrylandGame/Assets/Scripts/NPC/Character.cs
@@ -12,26 +12,14 @@ namespace NPC
         public TextAsset DialogXML;
         public Conversations conversations;
 
-        void Start()
+        void Awake()
         {
             string xmlString = DialogXML.text;
-
             XmlSerializer serializer = new XmlSerializer(typeof(Conversations));
 
             using (StringReader reader = new StringReader(xmlString))
             {
                 this.conversations = (Conversations)serializer.Deserialize(reader);
-                
-                //  Debug
-                foreach (var conversation in conversations.ConversationList)
-                {
-                    Debug.Log("Day: " + conversation.Day);
-                    foreach (var dialog in conversation.Dialogs)
-                    {
-                        Debug.Log("Phrase: " + dialog.Phrase);
-                        Debug.Log("Answer: " + dialog.Answer);
-                    }
-                }
             }
         }
     }
